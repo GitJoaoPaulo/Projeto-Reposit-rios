7b6a1328900ba44385ce6782cef28130921cb9da
GitJoaoPaulo <joaopaulobt@outlook.com>
2022-09-14 21:10:38 -0300

    Criação da página Main

--- repositorios/src/pages/Main/styles.js
+++ repositorios/src/pages/Main/styles.js
@@ -1,5 +1,69 @@
 import styled from "styled-components";
 
-export const Title = styled.h1`
-    color: #FF0000;
+export const Container = styled.div`
+    max-width: 700px;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 30px;
+    margin: 80px auto;
+    box-shadow: 0 0 20px rgba(0,0,0,0.2);
+
+    h1{
+        font-size: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        svg{
+            margin-right: 10px;
+        }
+    }
+
+    @media screen and (max-width: 720px){
+        width: 90%;
+    }
+`;
+
+export const Form = styled.form`
+    margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+
+    input{
+        flex: 1;
+        border: 1px solid #DDD;
+        padding: 10px 15px;
+        border-radius: 5px;
+        font-size: 17px;
+    }
+
+    @media screen and (max-width: 390px){
+        flex-direction: column;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+         
+        input{
+            width: 90%;
+            margin-bottom: 10px;
+        }
+
+    }
+`;
+
+export const SubmitButton = styled.button`
+    background-color: #0D2636;
+    border: 0;
+    margin-left: 10px;
+    padding: 0  15px;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    @media screen and (max-width: 390px){
+        width: 90%;
+        height: 30px;
+        margin: 0;
+    }
 `;
