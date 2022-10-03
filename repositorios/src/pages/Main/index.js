575438368bb771b49c96b7bccb8099b9aef50863
GitJoaoPaulo <joaopaulobt@outlook.com>
2022-10-03 18:29:38 -0300

    Listando os repositórios

--- repositorios/src/pages/Main/index.js
+++ repositorios/src/pages/Main/index.js
@@ -1,13 +1,13 @@
 import React, { useState, useCallback } from 'react';
 import api from '../../service/api';
-import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 
 export default function Main() {
 
   const [newRepo, setNewRepo] = useState('');
   const [loading, setLoading] = useState(false);
-  const [repository, setRepository] = useState('');
+  const [repository, setRepository] = useState([]);
 
   const handleSubmit = useCallback((e) => {
     e.preventDefault()
@@ -44,6 +44,12 @@ export default function Main() {
     setNewRepo(e.target.value);
   }
 
+  const handleDelete = useCallback((repo) =>{
+    const find = repository.filter(r => r.name !== repo);
+    setRepository(find);
+
+  }, [repository])
+
 
 
   return (
@@ -66,6 +72,23 @@ export default function Main() {
         </SubmitButton>
 
       </Form>
+
+      <List>
+        {repository.map(repo => (
+          <li key={repo.name}>
+            <span>
+              <DeleteButton onClick= {() => handleDelete(repo.name)}>
+                <FaTrash size={14}/>
+              </DeleteButton>
+
+              {repo.name}
+            </span>
+            <a href="">
+              <FaBars size={20} />
+            </a>
+          </li>
+        ))}
+      </List>
 
     </Container>
 
